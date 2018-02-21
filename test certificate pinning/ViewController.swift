48bc06615588812a73213ae2c6eadbe0f171d22f
Unknown <luciobotteri@gmail.com>
2018-02-21 16:35:11 +0100

    Button

--- test certificate pinning/ViewController.swift
+++ test certificate pinning/ViewController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //commit di prova Lucio
+    
+    @IBOutlet weak var provaButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
     }
-
-
 }
-
